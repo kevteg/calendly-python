288d48f01bee57d486c76ddcfba7ba021bf88f64
Kevin Hernandez <kevteg05@gmail.com>
2019-06-10 15:26:09 -0400

    headers variable and returning response

--- utils/requests.py
+++ utils/requests.py
@@ -4,26 +4,22 @@ import json
 
 class CaRequest(object):
 
-    def __init__(auth):
-        self.auth = auth
+    def __init__(self, token):
+        self.headers = {'X-TOKEN': token}
 
-    def process_request(method, url, data=None, headers=None):
-        #_ = {}
-        #if data:
-        #    _['data'] = json.dumps(kwargs["data"])
-
+    def process_request(self, method, url, data=None):
         request_method = getattr(requests, method)
-        return request_method(url, data, auth=self.auth)
+        return request_method(url, json=data, headers=self.headers)
 
     def get(self, url, data=None):
-        self.process_request('get', url, data)
+        return self.process_request('get', url, data)
 
     def post(self, url, data=None):
-        self.process_request('post', url, data)
+        return self.process_request('post', url, data)
 
     def delete(self, url, data=None):
-        self.process_request('delete', url, data)
+        return self.process_request('delete', url, data)
 
     def put(self, url, data=None):
-        self.process_request('put', url, data)
+        return self.process_request('put', url, data)
 
